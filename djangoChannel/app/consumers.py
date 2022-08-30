09a10fb7db573199c2b4aa3827010bc8fc66f248
faraz455 <khanfaraz1199@gmail.com>
2022-08-30 12:33:58 +0500

    Channel Layer redis and Multi user chat log

--- djangoChannel/app/consumers.py
+++ djangoChannel/app/consumers.py
@@ -1,28 +1,53 @@
 from channels.consumer import SyncConsumer
 from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
+from asgiref.sync import async_to_sync
 import time
 import asyncio
+import json
+
 class MySyncConsumer(SyncConsumer):
 
     def websocket_connect(self, event):
         print('Websocket connect...', event)
+
+        async_to_sync (self.channel_layer.group_add)(
+            'channel_group',
+            self.channel_name
+            )
 
         self.send({
             'type': 'websocket.accept'
         })
 
     def websocket_receive(self,event):
-        print('Message recieved...', event)
-        for i in range(50):
-            self.send({
-                'type' : 'websocket.send',
-                'text' : str(i)
-            })
-            time.sleep(1)
-    
+        print('Message recieved...', event['text'])
+        # for i in range(50):
+        #     self.send({
+        #         'type' : 'websocket.send',
+        #         'text' : json.dumps({'count': i})
+        #     })
+        #     time.sleep(1)
+        async_to_sync (self.channel_layer.group_send)(
+            'channel_group', 
+                {
+                'type': 'chat.message',
+                'message': event['text']
+                }
+            )
+    def chat_message(self, event):
+        self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
+
     def websocket_disconnect(self,event):
         print('Websocket disconnect...', event)
+
+        async_to_sync (self.channel_layer.group_discard)(
+            'channel_group', 
+            self.channel_name
+            )
         raise StopConsumer()
 
 class MyAsyncConsumer(AsyncConsumer):
