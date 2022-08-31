0e5fce00830b24414aa8103a241abb41cfaba8cf
faraz455 <khanfaraz1199@gmail.com>
2022-08-31 12:26:30 +0500

    unwanted prints removed

--- djangoChannel/app/views.py
+++ djangoChannel/app/views.py
@@ -2,16 +2,12 @@ from django.shortcuts import render
 from app.models import Chat, Group
 # Create your views here.
 def indexPage(request, group_name):
-    print(group_name, 'group nameeeee')
     group = Group.objects.filter(name=group_name).first()
     chats= []
     if group:
         chats = Chat.objects.filter(groupName = group)
-        print('in chatttttt---- ', chats)
     else:
         group = Group(name = group_name)
         group.save()
-    print('group----------', group)
     
-
     return render(request, 'index.html', {'groupName': group_name, 'chats': chats})
